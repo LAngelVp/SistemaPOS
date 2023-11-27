e29649c17d463a6bcc6b4dd735482a1bc20c10e3
Luis <angelvallejop9610@gmail.com>
2023-11-26 22:19:02 -0600

    Cree la ventana de registro y verificacion de usuario

--- Project/Clases/Base_de_Datos/Conection.py
+++ Project/Clases/Base_de_Datos/Conection.py
@@ -48,6 +48,8 @@ class ConexionBD:
     def crear_tablas(self):
         conexion = self.conexion
         cursor = conexion.cursor()
+
+        # COMMENT: TABLA DE PROVEEDOR
         tabla_proveedor = """CREATE TABLE IF NOT EXISTS proveedores(
         id INTEGER PRIMARY KEY,
         nombre VARCHAR(255),
@@ -61,6 +63,40 @@ class ConexionBD:
         lista_productos_suministrados FLOAT,
         documentos_adjuntos BLOB
         )"""
+        # COMMENT: TABLA PUESTOS
+        tabla_puesto = """CREATE TABLE IF NOT EXISTS puestos(
+        id INTEGER PRIMARY KEY,
+        nombre_puesto VARCHAR(100),
+        sueldo FLOAT
+        )"""
+        # COMMENT: TABLA USUARIOS
+        tabla_usuario = """CREATE TABLE IF NOT EXISTS usuarios (
+        id INTEGER PRIMARY KEY,
+        nombre_usuario VARCHAR(50),
+        contraseña VARCHAR(25)
+        )"""
+        # COMMENT: TABLA CONTACTO EMERGENCIA
+        tabla_contacto_emergencia = """CREATE TABLE IF NOT EXISTS contactos_emergencia (
+        id INTEGER PRIMARY KEY,
+        nombre VARCHAR(255),
+        parentezco VARCHAR(50),
+        numero_telefono VARCHAR(10)
+        )"""
+        
+        # ---------------------------------------------------------------
+        # COMMENT: TABLAS CON LLAVES FORANEAS ---------------------------
+        # COMMENT: TABLA DE COMPRAS
+        tabla_compras = """CREATE TABLE IF NOT EXISTS compras(
+        id INTEGER PRIMARY KEY,
+        fecha_compra DATE,
+        id_proveedor INTEGER,
+        total_compra FLOAT,
+        metodo_pago VARCHAR(50),
+        numero_factura VARCHAR(100),
+        comentarios TEXT,
+        FOREIGN KEY (id_proveedor) REFERENCES proveedores(id)
+        )"""
+        # COMMENT: TABLA PRODUCTOS
         tabla_productos = """CREATE TABLE IF NOT EXISTS productos(
         id INTEGER PRIMARY KEY,
         codigo_barras VARCHAR(255),
@@ -74,17 +110,17 @@ class ConexionBD:
         id_proveedor INTEGER,
         FOREIGN KEY (id_proveedor) REFERENCES proveedores(id)
         )"""
-        tabla_usuario = """CREATE TABLE IF NOT EXISTS usuarios (
+        #COMMENT: TABLA DE DETALLE DE COMPRA
+        tabla_detalle_compra = """CREATE TABLE IF NOT EXISTS detalle_compra(
         id INTEGER PRIMARY KEY,
-        nombre_usuario VARCHAR(50),
-        contraseña VARCHAR(25)
+        id_compra INTEGER,
+        id_producto INTEGER,
+        cantidad INTEGER,
+        precio_unitario FLOAT,
+        FOREIGN KEY (id_compra) REFERENCES compras(id),
+        FOREIGN KEY (id_producto) REFERENCES productos(id)
         )"""
-        tabla_contacto_emergencia = """CREATE TABLE IF NOT EXISTS contactos_emergencia (
-        id INTEGER PRIMARY KEY,
-        nombre VARCHAR(255),
-        parentezco VARCHAR(50),
-        numero_telefono VARCHAR(10)
-        )"""
+        # COMMENT: TABLA EMPLEADOS
         tabla_empleados = """CREATE TABLE IF NOT EXISTS empleados(
         id INTEGER PRIMARY KEY,
         nombre VARCHAR(255),
@@ -106,17 +142,19 @@ class ConexionBD:
         FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
         FOREIGN KEY (contacto_emergencia_id) REFERENCES contactos_emergencia(id)
         )"""
-        tabla_puesto = """CREATE TABLE IF NOT EXISTS puestos(
-        id INTEGER PRIMARY KEY,
-        nombre_puesto VARCHAR(100),
-        sueldo FLOAT
-        )"""
+        #--------------------------------------------------
+        #COMMENT: EJECUCION DE LAS TABLAS SIN LLAVE FORANEA
         cursor.execute(tabla_proveedor)
-        cursor.execute(tabla_productos)
-        cursor.execute(tabla_contacto_emergencia)
-        cursor.execute(tabla_usuario)
         cursor.execute(tabla_puesto)
+        cursor.execute(tabla_usuario)
+        cursor.execute(tabla_contacto_emergencia)
+        #---------------------------------------------------
+        #COMMENT: EJECUCION DE LAS TABLAS CON LLAVES FORANEAS
+        cursor.execute(tabla_productos)
+        cursor.execute(tabla_compras)
+        cursor.execute(tabla_detalle_compra)
         cursor.execute(tabla_empleados)
+
         self.conexion.commit()
         self.cerrar_conexion
 
